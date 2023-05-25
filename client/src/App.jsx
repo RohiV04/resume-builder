a5c4c60199990db6320686b31f22d4a45523cafd
Rohith <95613210+RohiV04@users.noreply.github.com>
2023-05-25 14:10:25 +0530

    Updated Landing Page Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import React from 'react'
+import { useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-
-import Resume from './Resume'
-import Landing from './Landing'
+import Resume from "./Resume";
+import Landing from "./Landing";
 function App() {
-
-
   return (
     <>
-    {/* <Landing/> */}
-<Resume/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+
+          <Route path="/Resume" element={<Resume />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
