4f3f9a4fe3dc4ac3506176078e77731eec269255
Rohith <95613210+RohiV04@users.noreply.github.com>
2023-06-09 10:00:10 +0530

    http

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -14,7 +14,7 @@ const LoginForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('htpp://localhost:8000/auth/login', formData);
+      const response = await axios.post('http://localhost:8000/auth/login', formData);
       console.log(response.data);
       // Reset the form
       setFormData({ email: '', password: '' });
